378c9552704c9f49deb7b50796895de8c307d23d
Alonso181006 <alonsomartin181006@gmail.com>
2025-07-24 20:36:15 -0700

    Import Yeti MVP URL
    Logged in to Import Yeti, and added stealth package to avoid detection, and scraped the importYeti URL of company

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();           
 const fs = require('fs');
 const csv = require('csv-parser');
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const { OpenAI } = require("openai");
 const path = require('path');
 
@@ -46,6 +48,16 @@ const openAIClient = new OpenAI({apiKey:process.env.OPEN_API_KEY});
     const page = await browser.newPage();
 
     // 2. Log in
+        //Yeti
+    await page.goto('https://www.importyeti.com/login', { waitUntil: 'networkidle2' });
+    await page.type('#email', process.env.YETI_USER);
+    await page.type('#password', process.env.YETI_PASS);
+    await Promise.all([
+    page.click('button[type="submit"]'),
+    page.waitForNavigation({ waitUntil: 'networkidle2' })
+    ]);
+
+        // Rev Vessel
     await page.goto('https://app.revenuevessel.com/login', { waitUntil: 'networkidle2' });
     await page.type('#email', process.env.RV_USER);
     await page.type('#password', process.env.RV_PASS);
@@ -56,15 +68,18 @@ const openAIClient = new OpenAI({apiKey:process.env.OPEN_API_KEY});
 
     console.log('Logged in');
 
+
     // 3. Loop over domains
-    const typeBoxSelector = 'input[placeholder="Search by company or domain name"]';
-    const linkSelector = 'table tbody tr td a.text-indigo-600';
+    const typeBoxRV = 'input[placeholder="Search by company or domain name"]';
+    const typeBoxYeti = "input[placeholder=\"Find Any Company's Suppliers\"]";
+    const linkSelectorRV = 'table tbody tr td a.text-indigo-600';
     const results = [];
 
     for (let i = 0; i < companies.length; i++) {
         const { domain, companyName, legalName } = companies[i];
 
-        let url = '';
+        let urlRV = "";
+        let urlYeti = "";
         let customsBrokersStr = "";
         let totalShipments;
         let totalClearances;
@@ -72,32 +87,33 @@ const openAIClient = new OpenAI({apiKey:process.env.OPEN_API_KEY});
         let portTypeStr = "";
 
         // Domain Check
-        await clearAndType(page, typeBoxSelector, domain);
-        url = await getImporterDetailURL(page, linkSelector);
+        await clearAndType(page, typeBoxRV, domain);
+        urlRV = await getImporterDetailURL(page, linkSelectorRV);
         
         const searchVariants = [legalName, companyName, companyName.replace(/\s+/g, ''), legalName.replace(/\s+/g, '')];
-        if (!url) {
+        if (!urlRV) {
             await page.goto('https://app.revenuevessel.com/dashboard/importers', { waitUntil: 'networkidle2' });
 
             for (const term of searchVariants) {
-                // if (url) break; // Already found a match
-                await clearAndType(page, typeBoxSelector, term);
-                url = await getImporterDetailURL(page, linkSelector);
+                if (urlRV) break; // Already found a match
+                await clearAndType(page, typeBoxRV, term);
+                urlRV = await getImporterDetailURL(page, linkSelectorRV);
             }
         }        
 
-        if (!url) {
-            await page.goto('https://app.revenuevessel.com/dashboard/importers?companyNameSearchStrategy=Fuzzy&', { waitUntil: 'networkidle2' });
+        // Fuzzy Seach !!!
+        // if (!url) {
+        //     await page.goto('https://app.revenuevessel.com/dashboard/importers?companyNameSearchStrategy=Fuzzy&', { waitUntil: 'networkidle2' });
 
-            for (const term of searchVariants) {
-                if (url) break; // Already found a match
-                await clearAndType(page, typeBoxSelector, term);
-                url = await getImporterDetailURL(page, linkSelector);
-            }
-        }
+        //     for (const term of searchVariants) {
+        //         if (url) break; // Already found a match
+        //         await clearAndType(page, typeBoxRV, term);
+        //         url = await getImporterDetailURL(page, linkSelector);
+        //     }
+        // }
 
-        if(url) {
-            await page.goto(url, { waitUntil: 'networkidle2' });
+        if(urlRV) {
+            await page.goto(urlRV, { waitUntil: 'networkidle2' });
             await page.waitForSelector("nav button", { timeout: 5000 });
 
             // Summary Scraping
@@ -301,40 +317,51 @@ const openAIClient = new OpenAI({apiKey:process.env.OPEN_API_KEY});
                     })
                 });
 
-                sortStringifyArray(customsBrokers, 'name', 'total', true);
+                customsBrokersStr = await sortStringifyArray(customsBrokers, 'name', 'total', true);
 
                 console.log(customsBrokersStr);
             } catch (err) {
                 console.warn(`Skipping Scraping Customs Data from ${domain} because: ${err.message}`);
             }
         }
-        console.log("Hello")
-        console.log(companyName, url, totalClearances, totalShipments, customsBrokersStr, tradeDataStr, portTypeStr);
-        results.push({companyName, url, totalClearances, totalShipments, customsBrokersStr, tradeDataStr, portTypeStr});
-        await page.goto('https://app.revenuevessel.com/dashboard/importers', { waitUntil: 'networkidle2' });
+        // Import Yeti
+        await page.goto('https://www.importyeti.com', { waitUntil: 'networkidle2' });
+
+        for (const term of searchVariants) {
+            if (urlYeti) break; // Already found a match
+            await clearAndType(page, typeBoxYeti, term);
+            await page.click('button[aria-label="Search button"]', {timeout: 10000});
+            urlYeti = await findYetiLink(page);
+            console.log("Url:", urlYeti)
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+        }
+
+        console.log(companyName, urlRV, urlYeti, totalClearances, totalShipments, customsBrokersStr, tradeDataStr, portTypeStr);
+        results.push({companyName, urlRV, urlYeti, totalClearances, totalShipments, customsBrokersStr, tradeDataStr, portTypeStr});
+
+        await page.goto('https://app.revenuevessel.com/dashboard/importers', { waitUntil: 'networkidle2'});
     }
+
     // 4. (For now) just close
     await browser.close();
 
     // 5. Write results to CSV
-    const header = 'Name,Url,Total Customs Clearances,Total Shipments,Customs Broker,Trade Data, Port Type\n';
+    const header = 'Name,UrlRV,URLYeti,Total Customs Clearances,Total Shipments,Customs Broker,Trade Data, Port Type\n';
 
     const csvLines = results.map(r => {
         const name = (r?.companyName || '').toString().replace(/"/g, '""');
-        const url = (r?.url || '').toString().replace(/"/g, '""');
+        const urlRV = (r?.urlRV || '').toString().replace(/"/g, '""');
+        const urlYeti = (r?.urlYeti || '').toString().replace(/"/g, '""');
 
         const totalClearances = (r?.totalClearances || '').toString().replace(/,/g, '').replace(/"/g, '""');
-
         const totalShipments = (r?.totalShipments || '').toString().replace(/,/g, '').replace(/"/g, '""');
-
         const customsBroker = (r?.customsBrokersStr || '').replace(/"/g, '""');
-
         const tradeData = (r?.tradeDataString || '').replace(/"/g, '""');
-
         const portTypeData = (r?.portTypeStr || '').replace(/"/g, '""');
 
 
-    return `"${name}","${url}",${totalClearances},${totalShipments},"${customsBroker}","${tradeData}", "${portTypeData}"`;
+    return `"${name}","${urlRV}","${urlYeti}",${totalClearances},${totalShipments},"${customsBroker}","${tradeData}", "${portTypeData}"`;
     });
 
     fs.writeFileSync('results.csv', header + csvLines.join('\n'));
@@ -342,40 +369,40 @@ const openAIClient = new OpenAI({apiKey:process.env.OPEN_API_KEY});
 })();
 
 async function clearAndType(page, selector, value) {
-  await page.click(selector, { clickCount: 3 });
-  await page.keyboard.press('Backspace');
-  console.log(value);
-  await page.type(selector, value);
-  await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.click(selector, { clickCount: 3 });
+    await page.keyboard.press('Backspace');
+    console.log(value);
+    await page.type(selector, value);
+    await new Promise(resolve => setTimeout(resolve, 2000));
 }
 
 async function getImporterDetailURL(page, selector) {
-  try {
-    await page.waitForSelector(selector, { visible: true, timeout: 5000 });
-    const url = await page.$eval(selector, a => a.href);
-    console.log('Importer detail URL:', url);
-    return url;
-  } catch {
-    console.log("No results");
-    return url = '';
-  }
+    try {
+        await page.waitForSelector(selector, { visible: true, timeout: 5000 });
+        const url = await page.$eval(selector, a => a.href);
+        console.log('Importer detail URL:', url);
+        return url;
+    } catch (err) {
+        console.log("No results:", err.message,); 
+        return url = '';
+    }
 }
 
 
-async function getTradeLanesData(activePage, tab, domain, column1, column2) {
+async function getTradeLanesData(page, tab, domain, column1, column2) {
     try {
-        await activePage.evaluate((tabName) => {
+        await page.evaluate((tabName) => {
             const btn = document.querySelector(`button[aria-controls*="${tabName}"]`);
             if (!btn || btn.disabled) {
                 throw new Error(tabName + " not active");
             }
         }, tab);
-        await activePage.click(`button[aria-controls*="${tab}"]`);
+        await page.click(`button[aria-controls*="${tab}"]`);
         
-        await activePage.waitForSelector('div[data-state="active"] table tbody tr', {visible: true, timeout: 5000});
+        await page.waitForSelector('div[data-state="active"] table tbody tr', {visible: true, timeout: 5000});
 
         // Scrape port + shipments per row
-        const scrapedRows = await activePage.$$eval('div[data-state="active"] table tbody tr',
+        const scrapedRows = await page.$$eval('div[data-state="active"] table tbody tr',
             (rows, col1, col2) => rows.map(row => {
                 const tds = row.querySelectorAll('td');
                 return {
@@ -418,4 +445,19 @@ async function sortStringifyArray(inputArray, key1, key2, notportType) {
         const value = entry[key2] || '';
         return `${label}-${value}`;
     }).join(', ');
+}
+
+async function findYetiLink(page) {
+    try {
+        await page.waitForSelector('.bg-yeti-neutral-05.hover\\:bg-yeti-neutral-10', { visible: true, timeout: 5000 });
+        return await page.$$eval('.bg-yeti-neutral-05.hover\\:bg-yeti-neutral-10', companies => {
+            const match = companies.find(company => company.closest("div")?.querySelector(".fflag.ff-sm.fflag-US"));
+            if (!match) return '';
+            const anchor = match.querySelector("a[href]");
+            return anchor ? anchor.href : '';
+        });
+    } catch (err) {
+        console.log("No results:", err.message); 
+        return '';
+    }
 }
